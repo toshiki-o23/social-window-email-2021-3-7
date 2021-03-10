d6a80e66967ca9bdf5dd1b9e63d6e738aa0b20e7
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-03-10 18:48:46 +0900

    Add mailer

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -4,27 +4,26 @@ class InquiriesController < ApplicationController
   end
 
   def confirm
-
-    # ==========ここから追加==========
     @inquiry = Inquiry.new(inquiry_params)
     if @inquiry.save
       render "confirm"
     else
       render :new
     end
-    # ==========ここまで追加==========
-
   end
 
   def thanks
+
+    # ==========ここから追加する==========
+    @inquiry = Inquiry.new(inquiry_params)
+    InquiryMailer.received_email(@inquiry).deliver_now
+    # ==========ここまで追加する==========
+
   end
 
-  # ==========ここから追加==========
   private
 
-    def inquiry_params
-      params.require(:inquiry).permit(:name, :email, :relationship, :content)
-    end
-  # ==========ここまで追加==========
-
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :relationship, :content)
+  end
 end
