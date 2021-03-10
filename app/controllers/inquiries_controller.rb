6648ad25bec97c949839bde547cfa4e05f1c77b2
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-03-10 18:35:06 +0900

    Add form

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,10 +1,30 @@
 class InquiriesController < ApplicationController
   def new
+    @inquiry = Inquiry.new;
   end
 
   def confirm
+
+    # ==========ここから追加==========
+    @inquiry = Inquiry.new(inquiry_params)
+    if @inquiry.save
+      render "confirm"
+    else
+      render :new
+    end
+    # ==========ここまで追加==========
+
   end
 
   def thanks
   end
+
+  # ==========ここから追加==========
+  private
+
+    def inquiry_params
+      params.require(:inquiry).permit(:name, :email, :relationship, :content)
+    end
+  # ==========ここまで追加==========
+
 end
